63ac4c2111641bd55dcfdca62f5e2ff5292c4db9
Andrey Kupryaev <kaa.013@mail.ru>
2026-03-25 21:30:37 +0300

    package main
    import (
    	"errors"
    	"fmt"
    )
    
    func secondMax(arr []int) (int, error) {
    	if len(arr) < 2 {
    		return 0, errors.New("что-то пошло не так")
    	}
    
    	max, second := arr[0], arr[1]
    
    	if second > max {
    		max, second = second, max
    	}
    
    	for _, num := range arr[2:] {
    		if num > max {
    			second = max
    			max = num
    		} else if num > second && num != max {
    			second = num
    		}
    	}
    
    	return second, nil
    }
    
    func main() {
    	res, err := secondMax([]int{10, 20, 4, 45, 99})
    	if err != nil {
    		fmt.Println("ошибка:", err)
    	} else {
    		fmt.Println(res)
    	}
    
    	res, err = secondMax([]int{5})
    	if err != nil {
    		fmt.Println("ошибка:", err)
    	} else {
    		fmt.Println(res)
    	}
    }

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
+func mergeAndSort(arr1, arr2 []int) []int {
+
+	result := append(arr1, arr2...)
+
+	sort.Ints(result)
+
+	return result
+}
+
+func main1() {
+	fmt.Println(mergeAndSort([]int{3, 1, 5}, []int{4, 2, 6})) // [1 2 3 4 5 6]
+	fmt.Println(mergeAndSort([]int{8, 2, 0}, []int{7, 3, 1})) // [0 1 2 3 7 8]
 }
