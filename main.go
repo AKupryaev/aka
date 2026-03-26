d3ae2112fefc3730e065b362ae49063138d1b892
Andrey Kupryaev <kaa.013@mail.ru>
2026-03-26 23:54:29 +0300

    maxof3

--- main.go
+++ main.go
@@ -1,45 +1,20 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-func secondMax(arr []int) (int, error) {
-	if len(arr) < 2 {
-		return 0, errors.New("меньше двух элементов")
+func maxOfThree(a, b, c int) int {
+	if a >= b && a >= c {
+		return a
+	} else if b >= a && b >= c {
+		return b
+	} else {
+		return c
 	}
-
-	max, second := arr[0], arr[1]
-
-	if second > max {
-		max, second = second, max
-	}
-
-	for _, num := range arr[2:] {
-		if num > max {
-			second = max
-			max = num
-		} else if num > second && num != max {
-			second = num
-		}
-	}
-
-	return second, nil
 }
 
 func main() {
-	res, err := secondMax([]int{10, 20, 4, 45, 99})
-	if err != nil {
-		fmt.Println("ошибка:", err)
-	} else {
-		fmt.Println(res)
-	}
 
-	res, err = secondMax([]int{5})
-	if err != nil {
-		fmt.Println("ошибка:", err)
-	} else {
-		fmt.Println(res)
-	}
+	fmt.Println(maxOfThree(3, 7, 5))  // Output: 7
+	fmt.Println(maxOfThree(10, 2, 8)) // Output: 10
+
 }
