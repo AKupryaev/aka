9e253d4cdb0486da6e097a3b003645e5a82ea1dc
Andrey Kupryaev <kaa.013@mail.ru>
2026-04-04 23:02:43 +0300

    calculator

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
-func reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+func calculator(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		if b == 0 {
+			return 0
+		}
+		return a / b
+	default:
+		return 0
 	}
-	return string(runes)
 }
+
 func main() {
-	fmt.Println(reverse("hello"))  // olleh
-	fmt.Println(reverse("Привет")) // тевирП
+	fmt.Println(calculator(10, 5, "+")) // 15
+	fmt.Println(calculator(10, 5, "-")) // 5
+	fmt.Println(calculator(10, 5, "*")) // 50
+	fmt.Println(calculator(10, 0, "/")) // 0
 }
