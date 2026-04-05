03b883ea08756a6b81ba452d212b69a44229bcd5
Andrey Kupryaev <kaa.013@mail.ru>
2026-04-05 08:48:18 +0300

    sun1-n

--- main.go
+++ main.go
@@ -2,27 +2,16 @@ package main
 
 import "fmt"
 
-func calculator(a, b int, op string) int {
-	switch op {
-	case "+":
-		return a + b
-	case "-":
-		return a - b
-	case "*":
-		return a * b
-	case "/":
-		if b == 0 {
-			return 0
-		}
-		return a / b
-	default:
-		return 0
-	}
-}
+func sumTon(n int) int {
+	sum := 0
 
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum
+}
 func main() {
-	fmt.Println(calculator(10, 5, "+")) // 15
-	fmt.Println(calculator(10, 5, "-")) // 5
-	fmt.Println(calculator(10, 5, "*")) // 50
-	fmt.Println(calculator(10, 0, "/")) // 0
+
+	fmt.Println(sumTon(5))  //15
+	fmt.Println(sumTon(10)) //55
 }
