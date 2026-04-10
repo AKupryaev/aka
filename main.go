bdad249f18a0a4dfb01ad200c831e11de20ba649
Andrey Kupryaev <kaa.013@mail.ru>
2026-04-10 22:18:57 +0300

    swap*int

--- main.go
+++ main.go
@@ -1,22 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func powerFunc(exp int) func(int) int {
-	return func(base int) int {
-		return int(math.Pow(float64(base), float64(exp)))
-	}
+func swap(a, b *int) {
+	*a, *b = *b, *a
 }
 
 func main() {
-	square := powerFunc(2)
-	fmt.Println(square(4)) // 16
-	fmt.Println(square(5)) // 25
-
-	cube := powerFunc(3)
-	fmt.Println(cube(2)) // 8
-	fmt.Println(cube(3)) // 27
+	x, y := 10, 20
+	swap(&x, &y)
+	fmt.Println(x, y) // 20 10
 }
