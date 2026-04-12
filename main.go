4cd4ba912a480153f01c951e0581201bb3b34c7a
Andrey Kupryaev <kaa.013@mail.ru>
2026-04-12 19:12:52 +0300

    inventory

--- main.go
+++ main.go
@@ -1,19 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// объявление структуры
-type Rectangle struct {
-	Width  float64
-	Height float64
+// Структура товара
+type Product struct {
+	ID       int
+	Name     string
+	Price    float64
+	Quantity int
 }
 
-// метод для структуры
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
+// Структура склада
+type Inventory struct {
+	products map[int]Product
+}
+
+// Конструктор
+func NewInventory() *Inventory {
+	return &Inventory{
+		products: make(map[int]Product),
+	}
+}
+
+// Добавление товара
+func (inv *Inventory) AddProduct(p Product) {
+	inv.products[p.ID] = p
+}
+
+// Продажа товара
+func (inv *Inventory) SellProduct(id, qty int) error {
+	product, ok := inv.products[id]
+	if !ok {
+		return errors.New("товар не найден")
+	}
+
+	if product.Quantity < qty {
+		return errors.New("недостаточно товара")
+	}
+
+	product.Quantity -= qty
+	inv.products[id] = product // важно обновить!
+
+	return nil
 }
 
 func main() {
-	r := Rectangle{Width: 5, Height: 10}
-	fmt.Println(r.Area()) // 50
+	inv := NewInventory()
+
+	inv.AddProduct(Product{ID: 1, Name: "Телефон", Price: 30000, Quantity: 10})
+
+	fmt.Println(inv.SellProduct(1, 3)) // nil
+	fmt.Println(inv.SellProduct(1, 8)) // ошибка
+
+	fmt.Println(inv.products[1]) // проверка остатка
 }
