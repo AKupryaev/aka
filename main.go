91c53328aab19c371b364296f5541bd8c74d798c
Andrey Kupryaev <kaa.013@mail.ru>
2026-05-05 10:06:06 +0300

    semafor

--- main.go
+++ main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	go func() {
-		ch1 <- 1
-		ch2 <- 2
-	}()
 
-	for i := 0; i < 2; i++ {
+	sem := make(chan struct{}, 3) // три горутины могут работать одновременно
 
-		select {
-		case v := <-ch1:
-			fmt.Println("v =", v, "from ch1")
-		case v := <-ch2:
-			fmt.Println("v =", v, "from ch2")
+	for i := 0; i < 10; i++ {
+		go func(i int) {
 
-		}
+			sem <- struct{}{} // заняли слот
+
+			fmt.Println("start", i)
+			time.Sleep(1 * time.Second)
+			fmt.Println("end", i)
+
+			<-sem // освободили слот
+		}(i)
 	}
+
+	time.Sleep(5 * time.Second) // ждем, чтобы все горутины завершились
 }
