3d22364cac976f25924f7d2936dca7a38866538f
Andrey Kupryaev <kaa.013@mail.ru>
2026-05-05 12:41:18 +0300

    sync.Map в многопоточной среде

--- main.go
+++ main.go
@@ -6,13 +6,48 @@ import (
 )
 
 func main() {
+	var m sync.Map
 	var wg sync.WaitGroup
-	wg.Add(1)
 
-	go func() {
-		defer wg.Done() // забыли Done() без дефера зависнет навсегда
-		fmt.Println("работаю")
-	}()
+	const goroutines = 10
+	const ops = 1000
 
-	wg.Wait() // ✅ не зависнет навсегда
+	// запись (writers)
+	wg.Add(goroutines)
+	for i := 0; i < goroutines; i++ {
+		go func(id int) {
+			defer wg.Done()
+
+			for j := 0; j < ops; j++ {
+				m.Store(fmt.Sprintf("goroutine-%d-key-%d", id, j), j)
+			}
+		}(i)
+	}
+
+	// чтение (readers)
+	wg.Add(goroutines)
+	for i := 0; i < goroutines; i++ {
+		go func(id int) {
+			defer wg.Done()
+
+			for j := 0; j < ops; j++ {
+				key := fmt.Sprintf("goroutine-%d-key-%d", id, j)
+				value, ok := m.Load(key)
+				if ok {
+					_ = value
+				}
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
+	// итоговая проверка
+	count := 0
+	m.Range(func(key, value any) bool {
+		count++
+		return true
+	})
+
+	fmt.Println("Всего элементов:", count)
 }
